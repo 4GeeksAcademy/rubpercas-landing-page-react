dd2a43267cf405d713b0ec32903c2a0a026a5286
RubenPerezCastrillo <ruben.perez.castrillo@gmail.com>
2024-10-15 08:03:44 +0000

    NavBar changes

--- src/js/component/NavBar.jsx
+++ src/js/component/NavBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NavBar = () => {
     return (
-        <nav class="navbar navbar-expand-lg bg-black w-100">
+        <nav class="navbar navbar-expand-lg bg-black">
             <div class="container-fluid">
                 <a class="navbar-brand text-white" href="#">Start Bootstrap</a>
                 <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
